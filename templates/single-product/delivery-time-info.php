bcaa3a4c7ed3be69e17752ff3c53ff1c5c8c3473
vendidero <dennis.nissle@iwebix.de>
2015-05-04 21:33:34 +0200

    1.3.1

--- templates/single-product/delivery-time-info.php
+++ templates/single-product/delivery-time-info.php
@@ -14,4 +14,6 @@ global $product;
 
 <?php if ( $product->gzd_product->get_delivery_time_term() ) : ?>
 	<p class="wc-gzd-additional-info delivery-time-info"><?php echo $product->gzd_product->get_delivery_time_html();?></p>
+<?php elseif ( $product->is_type( 'variable' ) ) : ?>
+	<p class="wc-gzd-additional-info delivery-time-info"></p>
 <?php endif; ?>
