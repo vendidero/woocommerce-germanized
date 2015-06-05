88a11b4c98d0415335b5b679489dc7d60cadaa4b
vendidero <dennis.nissle@iwebix.de>
2015-06-05 20:52:13 +0200

    checkout parse

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,11 +1,14 @@
 jQuery( function( $ ) {
+	
 	$( document ).on( 'change', '.payment_methods input[name="payment_method"]', function() {
 		$( 'body' ).trigger( 'update_checkout' );
 	});
+	
 	$( 'body' ).bind( 'updated_checkout', function() {
 		if ( $( '.wc-gzd-place-order' ).length > 0 )
 			$( '.place-order:not(.wc-gzd-place-order)' ).remove();
 	});
+	
 	if ( $( '.woocommerce-checkout' ).find( '#order_review_heading' ).length > 0 ) {
 		$( '.woocommerce-checkout' ).find( '#payment' ).after( $( '.woocommerce-checkout' ).find( '#order_review_heading' ) ); 
 		$( '.woocommerce-checkout' ).find( '#order_review_heading' ).show();
