ce3f5255c390fd14432641696ce3db1b7c64620c
vendidero <dennis.nissle@iwebix.de>
2015-07-28 21:01:58 +0200

    theme compatibility checkout update js check

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,8 +1,12 @@
 jQuery( function( $ ) {
-	
-	$( document ).on( 'change', '.payment_methods input[name="payment_method"]', function() {
-		$( 'body' ).trigger( 'update_checkout' );
-	});
+
+	if ( $( '.payment_methods:first' ).parents( '#order_review' ).length ) {
+		
+		$( document ).on( 'change', '.payment_methods input[name="payment_method"]', function() {
+			$( 'body' ).trigger( 'update_checkout' );
+		});
+
+	}
 	
 	$( 'body' ).bind( 'updated_checkout', function() {
 		if ( $( '.wc-gzd-place-order' ).length > 0 )
