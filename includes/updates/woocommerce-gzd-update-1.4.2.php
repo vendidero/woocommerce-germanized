b7c5775bffc3666fd34eb7af6e56e55232394c22
vendidero <dennis.nissle@iwebix.de>
2015-11-10 10:19:44 +0100

    automatic update

--- includes/updates/woocommerce-gzd-update-1.4.2.php
+++ includes/updates/woocommerce-gzd-update-1.4.2.php
@@ -18,27 +18,3 @@ if ( get_option( 'woocommerce_gzd_trusted_review_reminder_days' ) ) {
 	update_option( 'woocommerce_gzd_trusted_shops_review_reminder_days', get_option( 'woocommerce_gzd_trusted_review_reminder_days' ) );
 	delete_option( 'woocommerce_gzd_trusted_review_reminder_days' );
 }
-
-/*
-$args = array(
-	'post_type' => array( 'product', 'product_variation' ),
-	'posts_per_page' => -1
-);
-
-$loop = new WP_Query( $args );
-
-if ( $loop->have_posts() ) {
-
-	while ( $loop->have_posts() ) {
-
-		global $post;
-		$loop->the_post(); 
-		$product = wc_get_product();
-
-		if ( $product->gzd_product->has_unit() && $product->gzd_product->unit_base )
-			update_post_meta( ( $product->is_type( 'variation' ) ? $product->variation_id : $product->id ), '_unit_product', round( $product->gzd_product->unit_base * 100, wc_get_price_decimals() ) );
-	}
-}
-
-wp_reset_postdata();
-*/
---
+++ includes/updates/woocommerce-gzd-update-1.4.6.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Update Germanized to 1.4.6
+ *
+ * @author 		WooThemes
+ * @category 	Admin
+ * @package 	WooCommerce Germanized/Updates
+ * @version     1.4.6
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( 'yes' === get_option( 'woocommerce_gzd_enable_virtual_vat' ) && 'base' === get_option( 'woocommerce_tax_based_on' ) )
+	update_option( 'woocommerce_tax_based_on', 'billing' );
