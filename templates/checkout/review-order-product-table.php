ec3d7b4e77d367fa90b7c1d55c9c0b81de40130e
vendidero <dennis.nissle@iwebix.de>
2016-02-11 18:07:01 +0100

    better thumbnail divs in checkout table

--- templates/checkout/review-order-product-table.php
+++ templates/checkout/review-order-product-table.php
@@ -17,17 +17,28 @@ foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 		?>
 		<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 			<td class="product-name">
+				
 				<?php if ( get_option( 'woocommerce_gzd_display_checkout_thumbnails' ) == 'yes' ) : ?>
+				
 					<div class="wc-gzd-product-name-left">
 						<?php echo apply_filters( 'woocommerce_cart_item_thumbnail', $_product->get_image(), $cart_item, $cart_item_key ); ?>
 					</div>
+				
+					<div class="wc-gzd-product-name-right">
+				
 				<?php endif; ?>
-				<div class="wc-gzd-product-name-right">
+				
 					<?php echo apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ); ?>
 					<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times; %s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); ?>
 					<?php echo WC()->cart->get_item_data( $cart_item ); ?>
-				</div>
-				<div class="clear"></div>
+				
+				<?php if ( get_option( 'woocommerce_gzd_display_checkout_thumbnails' ) == 'yes' ) : ?>
+
+					</div>
+					<div class="clear"></div>
+
+				<?php endif; ?>
+			
 			</td>
 			<td class="product-total">
 				<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); ?>
