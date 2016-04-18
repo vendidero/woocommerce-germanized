95e703e46fe5af1111b8cb4df1bed751c2a6ea05
vendidero <dennis.nissle@iwebix.de>
2016-04-18 13:15:58 +0200

    review link improvement

--- includes/admin/views/html-notice-review.php
+++ includes/admin/views/html-notice-review.php
@@ -16,7 +16,7 @@ $dismiss_url = add_query_arg( 'notice', 'wc-gzd-hide-review-notice', add_query_a
 		<?php _e( 'If you like WooCommerce Germanized and our Plugin does a good job it would be great if you would write a review about WooCommerce Germanized on WordPress.org. Thank you for your support!', 'woocommerce-germanized' ); ?>
 	</p>
 	<p class="alignleft wc-gzd-button-wrapper">
-		<a class="button button-primary" href="https://wordpress.org/support/view/plugin-reviews/woocommerce-germanized" target="_blank"><?php _e( 'Write review now', 'woocommerce-germanized' );?></a>
+		<a class="button button-primary" href="https://wordpress.org/support/view/plugin-reviews/woocommerce-germanized?rate=5#postform" target="_blank"><?php _e( 'Write review now', 'woocommerce-germanized' );?></a>
 		<a class="button button-secondary" href="https://wordpress.org/support/plugin/woocommerce-germanized" target="_blank"><?php _e( 'Found Bugs?', 'woocommerce-germanized' );?></a>
 	</p>
 	<p class="alignright">
