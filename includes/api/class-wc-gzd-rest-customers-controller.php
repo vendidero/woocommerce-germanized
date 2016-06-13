190d720c76c52677b9becfe72dcb49fdc73f1ad1
vendidero <dennis.nissle@iwebix.de>
2016-06-13 13:15:26 +0200

    rest api billing + shipping title support

--- includes/api/class-wc-gzd-rest-customers-controller.php
+++ includes/api/class-wc-gzd-rest-customers-controller.php
@@ -19,6 +19,83 @@ class WC_GZD_REST_Customers_Controller {
 	 */
 	public function __construct() {
 		$this->direct_debit_gateway = new WC_GZD_Gateway_Direct_Debit();
+
+		add_filter( 'woocommerce_rest_prepare_customer', array( $this, 'prepare' ), 10, 3 );
+		add_action( 'woocommerce_rest_insert_customer', array( $this, 'insert' ), 10, 3 );
+		add_filter( 'woocommerce_rest_customer_schema', array( $this, 'schema' ) );
+	}
+
+	/**
+	 * Filter customer data returned from the REST API.
+	 *
+	 * @since 1.0.0
+	 * @wp-hook woocommerce_rest_prepare_order
+	 *
+	 * @param \WP_REST_Response $response The response object.
+	 * @param \WP_User $customer User object used to create response.
+	 * @param \WP_REST_Request $request Request object.
+	 *
+	 * @return \WP_REST_Response
+	 */
+	public function prepare( $response, $customer, $request ) {
+
+		$response_customer_data = $response->get_data();
+		
+		$response_customer_data['billing_address']['title'] = $customer->billing_title;
+		$response_customer_data['shipping_address']['title'] = $customer->shipping_title;
+
+		$response->set_data( $response_customer_data );
+
+		return $response;
+	}
+
+	/**
+	 * Prepare a single customer for create or update.
+	 *
+	 * @since 1.0.0
+	 * @wp-hook woocommerce_rest_insert_customer
+	 *
+	 * @param \WP_User $customer Data used to create the customer.
+	 * @param \WP_REST_Request $request Request object.
+	 * @param bool $creating True when creating item, false when updating.
+	 */
+	public function insert( $customer, $request, $creating ) {
+
+		if ( isset( $request['billing']['title'] ) ) {
+			update_user_meta( $customer->ID, '_billing_title', absint( $request['billing']['title'] ) );
+		}
+
+		if ( isset( $request['shipping']['title'] ) ) {
+			update_user_meta( $customer->ID, '_shipping_title', absint( $request['shipping']['title'] ) );
+		}
+
+	}
+
+	/**
+	 * Extend schema.
+	 *
+	 * @since 1.0.0
+	 * @wp-hook woocommerce_rest_customer_schema
+	 *
+	 * @param array $schema_properties Data used to create the customer.
+	 *
+	 * @return array
+	 */
+	public function schema( $schema_properties ) {
+
+		$schema_properties['billing']['properties']['title'] = array(
+			'description' => __( 'Title', 'woocommerce-germanized-pro' ),
+			'type'        => 'string',
+			'context'     => array( 'view', 'edit' )
+		);
+
+		$schema_properties['shipping']['properties']['title'] = array(
+			'description' => __( 'Title', 'woocommerce-germanized-pro' ),
+			'type'        => 'string',
+			'context'     => array( 'view', 'edit' )
+		);
+
+		return $schema_properties;
 	}
 
 	/**
--- includes/api/class-wc-gzd-rest-orders-controller.php
+++ includes/api/class-wc-gzd-rest-orders-controller.php
@@ -19,6 +19,84 @@ class WC_GZD_REST_Orders_Controller {
 	 */
 	public function __construct() {
 		$this->direct_debit_gateway = new WC_GZD_Gateway_Direct_Debit();
+
+		add_filter( 'woocommerce_rest_prepare_shop_order', array( $this, 'prepare' ), 10, 3 );
+		add_action( 'woocommerce_rest_insert_shop_order', array( $this, 'insert' ), 10, 3 );
+		add_filter( 'woocommerce_rest_shop_order_schema', array( $this, 'schema' ) );
+	}
+
+	/**
+	 * Filter customer data returned from the REST API.
+	 *
+	 * @since 1.0.0
+	 * @wp-hook woocommerce_rest_prepare_order
+	 *
+	 * @param \WP_REST_Response $response The response object.
+	 * @param \WP_User $customer User object used to create response.
+	 * @param \WP_REST_Request $request Request object.
+	 *
+	 * @return \WP_REST_Response
+	 */
+	public function prepare( $response, $order, $request ) {
+
+		$order = wc_get_order( $post );
+		$response_order_data = $response->get_data();
+		
+		$response_order_data['billing']['title'] = $order->billing_title;
+		$response_order_data['shipping']['title'] = $order->shipping_title;
+		
+		$response->set_data( $response_order_data );
+
+		return $response;
+	}
+
+	/**
+	 * Prepare a single customer for create or update.
+	 *
+	 * @since 1.0.0
+	 * @wp-hook woocommerce_rest_insert_customer
+	 *
+	 * @param \WP_User $customer Data used to create the customer.
+	 * @param \WP_REST_Request $request Request object.
+	 * @param bool $creating True when creating item, false when updating.
+	 */
+	public function insert( $post, $request, $creating ) {
+
+		if ( isset( $request['billing']['title'] ) ) {
+			update_post_meta( $post->ID, '_billing_title', absint( $request['billing']['title'] ) );
+		}
+
+		if ( isset( $request['shipping']['title'] ) ) {
+			update_post_meta( $post->ID, '_shipping_title', absint( $request['shipping']['title'] ) );
+		}
+
+	}
+
+	/**
+	 * Extend schema.
+	 *
+	 * @since 1.0.0
+	 * @wp-hook woocommerce_rest_customer_schema
+	 *
+	 * @param array $schema_properties Data used to create the customer.
+	 *
+	 * @return array
+	 */
+	public function schema( $schema_properties ) {
+
+		$schema_properties['billing']['properties']['title'] = array(
+			'description' => __( 'Title', 'woocommerce-germanized-pro' ),
+			'type'        => 'string',
+			'context'     => array( 'view', 'edit' )
+		);
+
+		$schema_properties['shipping']['properties']['title'] = array(
+			'description' => __( 'Title', 'woocommerce-germanized-pro' ),
+			'type'        => 'string',
+			'context'     => array( 'view', 'edit' )
+		);
+
+		return $schema_properties;
 	}
 
 	/**
