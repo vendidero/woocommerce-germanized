6729cf88b54a753625da69159118fd829fbe6e9c
Dennis Nissle <info@vendidero.de>
2018-03-22 14:33:07 +0100

    Removed duplicate nonce from order-submit.php template to avoid duplicate id warnings. Adjusted button html to meet newest Woo version (changed from input to button element). Fixes #75

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,5 +1,4 @@
 jQuery( function( $ ) {
-
 	if ( $( '.payment_methods:first' ).parents( '#order_review' ).length ) {
 
 		$( document ).on( 'change', '.payment_methods input[name="payment_method"]', function() {
@@ -10,6 +9,10 @@ jQuery( function( $ ) {
 	
 	$( 'body' ).bind( 'updated_checkout', function() {
 		if ( $( '.wc-gzd-place-order' ).length > 0 ) {
+			if ( $( '.place-order' ).length > 0 ) {
+				// Make sure we are removing the nonce from the old container to the new one.
+                $( '.place-order' ).find( '#_wpnonce' ).appendTo( '.wc-gzd-place-order' );
+			}
 			$( '.place-order:not(.wc-gzd-place-order)' ).remove();
 		}
 	});
@@ -18,5 +21,4 @@ jQuery( function( $ ) {
 		$( '.woocommerce-checkout' ).find( '#payment' ).after( $( '.woocommerce-checkout' ).find( '#order_review_heading' ) ); 
 		$( '.woocommerce-checkout' ).find( '#order_review_heading' ).show();
 	}
-
 });
--- templates/checkout/order-submit.php
+++ templates/checkout/order-submit.php
@@ -15,18 +15,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="form-row place-order wc-gzd-place-order">
 
-		<noscript>
-			<?php _e( 'Since your browser does not support JavaScript, or it is disabled, please ensure you click the <em>Update Totals</em> button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce' ); ?>
-			<br/><input type="submit" class="button alt" name="woocommerce_checkout_update_totals" value="<?php esc_attr_e( 'Update totals', 'woocommerce' ); ?>" />
-		</noscript>
-
-		<?php wp_nonce_field( 'woocommerce-process_checkout' ); ?>
+        <noscript>
+			<?php esc_html_e( 'Since your browser does not support JavaScript, or it is disabled, please ensure you click the <em>Update Totals</em> button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce' ); ?>
+            <br/><button type="submit" class="button alt" name="woocommerce_checkout_update_totals" value="<?php esc_attr_e( 'Update totals', 'woocommerce' ); ?>"><?php esc_html_e( 'Update totals', 'woocommerce' ); ?></button>
+        </noscript>
 
 		<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
 
 		<?php do_action( 'woocommerce_gzd_review_order_before_submit' ); ?>
 
-		<?php echo apply_filters( 'woocommerce_order_button_html', '<input type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '" />' ); ?>
+		<?php echo apply_filters( 'woocommerce_order_button_html', '<button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); ?>
+
+        <?php // Do not insert another wpnonce here as the nonce will be appended via JS to avoid duplicate ids ?>
 
 		<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
 
