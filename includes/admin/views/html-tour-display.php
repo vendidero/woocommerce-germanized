1b818dadf7487e8e793edbd9bbecd41c5b6561ed
Dennis Nissle <info@vendidero.de>
2018-09-17 14:30:01 +0200

    fixed tour display

--- includes/admin/views/html-tour-display.php
+++ includes/admin/views/html-tour-display.php
@@ -67,38 +67,6 @@ $elements = array(
 		<a class="button button-primary tourbus-next" href="javascript:void(0);">weiter</a>
 	</li>
 
-	<li data-el="<?php echo $elements[ 'checkout_legal' ]; ?>" data-orientation="top">
-		<h2>Checkbox in der Kasse</h2>
-		<p>Einige Shops verzichten auf eine Checkbox zum Bestätigen der AGB und Widerrufsbelehrung und wählen stattdessen einen normalen Hinweis (s. z.B. Zalando).
-		Den Hinweistext bzw. Checkbox-Text kannst du über die folgenden Optionen steuern. Mit den Platzhaltern {term_link}, {revocation_link} und {data_security_link} kannst du Links zu den entsprechenden 
-		Rechtstexten einfügen (diese müssen natürlich in den Germanized Einstellungen unter <a href="<?php echo admin_url( 'admin.php?page=wc-settings&tab=germanized&section=display' ); ?>">Allgemein</a> hinterlegt sein).</p>
-		<a class="button button-primary tourbus-next" href="javascript:void(0);">weiter</a>
-	</li>
-
-	<li data-el="<?php echo $elements[ 'checkout_digital' ]; ?>" data-orientation="top">
-		<h2>Checkbox für digitale Produkte</h2>
-		<p>Solltest du digitale Produkte verkaufen, möchtest du vielleicht verhindern, dass Käufer dieser Produkte weiterhin über ein 14-tägiges Widerrufsrecht verfügen.
-		Sollte das der Fall sein, könnte jeder Käufer nach dem Download der Datei den Vertrag widerrufen. Aus diesem Grund, fügt Germanized eine Checkbox ein, die den Käufer
-		dazu auffordert, auf das Widerrufsrecht zur verzichten (standardmäßig nur dann, wenn sicher herunterladbare Produkte im Warenkorb befinden).</p>
-		<a class="button button-primary tourbus-next" href="javascript:void(0);">weiter</a>
-	</li>
-
-	<li data-el="<?php echo $elements[ 'digital_types' ]; ?>" data-orientation="top">
-		<h2>Digitale Produkttypen</h2>
-		<p>Standardmäßig wird der Hinweis bzgl. des Abtretens des Widerrufsrechts nur für herunterladbare Produkte angezeigt. Wenn du den Hinweis auch für andere Produkttypen (z.B. auch für virtuelle Produkte)
-		aktivieren möchtest, kannst du dies hier auswählen. Mit den Standard-Produkttypen (wie einfaches Produkt) solltest du allerdings vorsichtig sein, da der Hinweis dann immer angezeigt wird,
-		wenn ein einfaches Produkt im Warenkorb liegt (egal ob downloadbar/virtuell oder nicht).</p>
-		<a class="button button-primary tourbus-next" href="javascript:void(0);">weiter</a>
-	</li>
-
-	<li data-el="<?php echo $elements[ 'legal_service' ]; ?>" data-orientation="top">
-		<h2>Checkbox für Dienstleistungen</h2>
-		<p>Solltest du Dienstleistungen verkaufen, möchtest du vielleicht im Sinne des Käufers bereits vor Ablauf der Widerrufsfrist mit der Erfüllung der Dienstleistung beginnen. Für diesen Fall bieten Germanized die Option einer Checkbox an,
-		die es ermöglicht, den Käufer darauf hinzuweisen. Die Checkbox wird natürlich nur dann angezeigt wenn sich Dienstleistungen im Warenkorb befinden (d.h. Produkte die als Dienstleistung markiert wurden). Mehr Informationen dazu findest du auch beim <a href="https://www.haendlerbund.de/de/downloads/das-neue-widerrufsrecht-bei-dienstleistungen.pdf" target="_blank">Händlerbund</a>.
-		</p>
-		<a class="button button-primary tourbus-next" href="javascript:void(0);">weiter</a>
-	</li>
-
 	<li data-el="<?php echo $elements[ 'pay_now' ]; ?>" data-orientation="top">
 		<h2>Bezahlen-Button in E-Mails</h2>
 		<p>WooCommerce Germanized sorgt dafür, dass dem Käufer direkt nach der Bestellung (d.h. nach Betätigen des Kaufen-Buttons) eine Bestellbestätigung zugestellt wird. 
