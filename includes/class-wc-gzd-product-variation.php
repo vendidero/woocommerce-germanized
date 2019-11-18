56248b1836bef4d922275220d6efb7f43c5f20b9
Dennis Nissle <info@vendidero.de>
2019-11-18 12:39:51 +0100

    Make sure some variation data is forced to be inherited from parent (e.g. free_shipping).

--- includes/class-wc-gzd-product-variation.php
+++ includes/class-wc-gzd-product-variation.php
@@ -38,12 +38,23 @@ class WC_GZD_Product_Variation extends WC_GZD_Product {
 		'min_age'
 	);
 
+	protected $gzd_variation_forced_inherited_meta_data = array(
+		'unit',
+		'unit_base',
+		'free_shipping',
+		'differential_taxation',
+	);
+
 	public function get_gzd_parent() {
 		if ( is_null( $this->parent ) ) {
 			$this->parent = wc_gzd_get_product( $this->child->get_parent_id() );
 		}
 
 		return $this->parent;
+	}
+
+	public function get_forced_inherited_props() {
+		return $this->gzd_variation_forced_inherited_meta_data;
 	}
 
 	public function get_prop( $prop, $context = 'view' ) {
@@ -58,6 +69,11 @@ class WC_GZD_Product_Variation extends WC_GZD_Product {
 
 		} elseif ( in_array( $prop, $this->gzd_variation_inherited_meta_data ) ) {
 			$value = $this->child->get_meta( $meta_key, true, $context ) ? $this->child->get_meta( $meta_key, true, $context ) : '';
+
+			// Make sure forced inherited meta data (e.g. not choosable from admin view) is rejected if available
+			if ( in_array( $prop, $this->gzd_variation_forced_inherited_meta_data ) ) {
+				$value = '';
+			}
 
 			// Handle meta data keys which can be empty at variation level to cause inheritance
 			if ( 'view' === $context && ( ! $value || '' === $value ) ) {
@@ -93,5 +109,41 @@ class WC_GZD_Product_Variation extends WC_GZD_Product {
 
 		/** This filter is documented in includes/class-wc-gzd-product-variation.php */
 		return apply_filters( "woocommerce_gzd_get_product_variation_unit", $unit, $this, $this->child );
+	}
+
+	/**
+	 * Data is being handled by parent product.
+	 *
+	 * @param $unit
+	 */
+	public function set_unit( $unit ) {
+		parent::set_unit( '' );
+	}
+
+	/**
+	 * Data is being handled by parent product.
+	 *
+	 * @param $base
+	 */
+	public function set_unit_base( $base ) {
+		parent::set_unit_base( '' );
+	}
+
+	/**
+	 * Data is being handled by parent product.
+	 *
+	 * @param $free_shipping
+	 */
+	public function set_free_shipping( $free_shipping ) {
+		$this->set_prop( 'free_shipping', '' );
+	}
+
+	/**
+	 * Data is being handled by parent product.
+	 *
+	 * @param $diff_taxation
+	 */
+	public function set_differential_taxation( $diff_taxation ) {
+		$this->set_prop( 'differential_taxation', '' );
 	}
 }
