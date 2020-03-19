e3d4bcfb9384822e547f6a550af19ad09b3f5b07
Dennis Nissle <info@vendidero.de>
2020-03-19 17:23:00 +0100

    Show Germanized checkboxes during Klarna checkout. Use woocommerce_gzd_checkout_klarna_add_checkboxes to disable.

--- includes/compatibility/class-wc-gzd-compatibility-klarna-checkout-for-woocommerce.php
+++ includes/compatibility/class-wc-gzd-compatibility-klarna-checkout-for-woocommerce.php
@@ -23,6 +23,18 @@ class WC_GZD_Compatibility_Klarna_Checkout_For_WooCommerce extends WC_GZD_Compat
 		add_action( 'woocommerce_gzd_run_legal_checkboxes_checkout', array( $this, 'maybe_disable_checkboxes' ), 5 );
 		add_filter( 'woocommerce_gzd_enable_force_pay_order', array( $this, 'disable_force_pay_order' ), 10, 2 );
 		add_filter( 'kco_checkout_timeout_duration', array( $this, 'increase_checkout_time' ), 10 );
+
+		add_action( 'kco_wc_after_order_review', array( $this, 'add_checkboxes' ), 5 );
+	}
+
+	public function add_klarna_checkboxes() {
+		return apply_filters( 'woocommerce_gzd_checkout_klarna_add_checkboxes', true );
+	}
+
+	public function add_checkboxes() {
+		if ( $this->add_klarna_checkboxes() ) {
+			WC_GZD_Legal_Checkbox_Manager::instance()->render( 'checkout' );
+		}
 	}
 
 	public function increase_checkout_time( $time ) {
@@ -63,10 +75,12 @@ class WC_GZD_Compatibility_Klarna_Checkout_For_WooCommerce extends WC_GZD_Compat
 
 		if ( 'kco' === $payment_method ) {
 
-			$checkboxes = WC_GZD_Legal_Checkbox_Manager::instance()->get_checkboxes( array( 'locations' => 'checkout' ) );
+			if ( ! $this->add_klarna_checkboxes() ) {
+				$checkboxes = WC_GZD_Legal_Checkbox_Manager::instance()->get_checkboxes( array( 'locations' => 'checkout' ) );
 
-			foreach( $checkboxes as $checkbox ) {
-				$checkbox->set_is_mandatory( false );
+				foreach( $checkboxes as $checkbox ) {
+					$checkbox->set_is_mandatory( false );
+				}
 			}
 		}
 	}
