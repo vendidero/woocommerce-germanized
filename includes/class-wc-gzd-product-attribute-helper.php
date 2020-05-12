1219167befe93b2beb17bf05fca8b288a131b444
Dennis Nissle <info@vendidero.de>
2020-05-12 10:30:33 +0200

    further product obj check.

--- includes/class-wc-gzd-product-attribute-helper.php
+++ includes/class-wc-gzd-product-attribute-helper.php
@@ -72,7 +72,12 @@ class WC_GZD_Product_Attribute_Helper {
 	}
 
 	public function cart_item_data_filter( $item_data, $cart_item ) {
-		$cart_product      = $cart_item['data'];
+		$cart_product = $cart_item['data'];
+
+		if ( ! $cart_product ) {
+		    return $item_data;
+		}
+
 		$item_data_product = wc_gzd_get_gzd_product( $cart_product )->get_checkout_attributes( $item_data, isset( $cart_item['variation'] ) ? $cart_item['variation'] : array() );
 
 		if ( $item_data !== $item_data_product ) {
