8a2ed67b512baaec0f2e58e3066a756ee2c66237
Dennis Nissle <info@vendidero.de>
2020-09-08 15:32:38 +0200

    Adding /packages directory to release

---
+++ packages/woocommerce-trusted-shops/templates/trusted-shops/product-sticker-tpl.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Admin View: Product Sticker Template
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
+
+<script type="text/javascript">
+_tsProductReviewsConfig = {
+	tsid: '{id}', 
+	sku: ['{sku}'],
+	variant: 'productreviews',
+	borderColor: '{border_color}',
+    locale: '{locale}',
+    starColor: '{star_color}',
+    starSize: '{star_size}px',
+    ratingSummary: 'false',
+    maxHeight: '1200px',
+    element: '{element}',
+    hideEmptySticker: 'false',
+    richSnippets: 'off',
+    introtext: ''  /* optional */
+};
+var scripts = document.getElementsByTagName('SCRIPT'), me = scripts[scripts.length - 1];
+var _ts = document.createElement('SCRIPT');
+_ts.type = 'text/javascript';
+_ts.async = true;
+_ts.charset = 'utf-8';
+_ts.src ='//widgets.trustedshops.com/reviews/tsSticker/tsProductSticker.js';
+me.parentNode.insertBefore(_ts, me);
+_tsProductReviewsConfig.script = _ts;</script>
