b139f3d4a461ac953312adc9b39c68c44b55f634
Dennis Nissle <info@vendidero.de>
2021-06-28 17:11:05 +0200

    OSS installation check.

--- tests/unit-tests/util/install.php
+++ tests/unit-tests/util/install.php
@@ -60,6 +60,7 @@ class WC_GZD_Tests_Install extends WC_GZD_Unit_Test_Case {
 		// Check if package settings are added too (e.g. DHL, Shipments)
 		$this->assertEquals( 'yes', get_option( 'woocommerce_gzd_shipments_auto_enable' ) );
 		$this->assertEquals( 'V01PAK', wc_gzd_get_shipping_provider( 'dhl' )->get_setting( 'label_default_product_dom' ) );
+		$this->assertEquals( 'no', get_option( 'oss_use_oss_procedure' ) );
 
 		// Check if Tables are installed
 		global $wpdb;
