05cfe4e32c5fde235d14e3bee8d87002f3a2ff9b
Dennis Nissle <info@vendidero.de>
2021-06-28 17:29:53 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,159 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '64ae202a96736145c288b833df34546335100435',
+    'name' => 'vendidero/woocommerce-germanized',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.7.1',
+      'version' => '2.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5437697a56aefbdf707849b9833e1b36093d7a73',
+    ),
+    'baltpeter/internetmarke-php' => 
+    array (
+      'pretty_version' => 'v0.5.0',
+      'version' => '0.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7657ee5a55eb80c77a35e35ce71c465364d73ab4',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.11.0',
+      'version' => '1.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ae03311f45dfe194412081526be2e003960df74b',
+    ),
+    'dvdoug/boxpacker' => 
+    array (
+      'pretty_version' => '3.9.1',
+      'version' => '3.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '539084bd7b55056fa8b8b789e8ad5d3ebf5e5c3e',
+    ),
+    'myclabs/deep-copy' => 
+    array (
+      'pretty_version' => '1.10.2',
+      'version' => '1.10.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '776f831124e9c62e1a2c601ecc52e776d8bb7220',
+      'replaced' => 
+      array (
+        0 => '1.10.2',
+      ),
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'setasign/fpdf' => 
+    array (
+      'pretty_version' => '1.8.3',
+      'version' => '1.8.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a83253ece0df1c5b6c05fe7a900c660ae38afc3',
+    ),
+    'setasign/fpdi' => 
+    array (
+      'pretty_version' => 'v2.3.6',
+      'version' => '2.3.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6231e315f73e4f62d72b73f3d6d78ff0eed93c31',
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'vendidero/one-stop-shop-woocommerce' => 
+    array (
+      'pretty_version' => 'v1.0.5',
+      'version' => '1.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '16993cb7f9eb06f8e63014bf5ca33a93401471f0',
+    ),
+    'vendidero/woocommerce-germanized' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64ae202a96736145c288b833df34546335100435',
+    ),
+    'vendidero/woocommerce-germanized-dhl' => 
+    array (
+      'pretty_version' => 'v1.5.8',
+      'version' => '1.5.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5a6601ad98ca5abcefac84c8dab2b7dbb7374d20',
+    ),
+    'vendidero/woocommerce-germanized-shipments' => 
+    array (
+      'pretty_version' => 'v1.5.6',
+      'version' => '1.5.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd690e823fbbfdc7ab63122b47240dd62a21c95b0',
+    ),
+    'vendidero/woocommerce-trusted-shops' => 
+    array (
+      'pretty_version' => 'v4.0.11',
+      'version' => '4.0.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3b8ee60cd8dd1d54e1964085e29f8b63e4968c54',
+    ),
+    'wsdltophp/wssecurity' => 
+    array (
+      'pretty_version' => '1.2.2',
+      'version' => '1.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a450af3cd462cbf73fdb3a09e80322da893af4f',
+    ),
+  ),
+);
