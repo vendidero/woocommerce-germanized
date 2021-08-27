6225c9b983777c05435d038986f940a3f6bf62d8
Dennis Nissle <info@vendidero.de>
2021-08-27 13:05:51 +0200

    By default do not include privacy policy within general checkbox label as it does not need explicit consent. By default do not insert a separate privacy policy within customer registration forms and revocation form. The privacy policy should be linked in the footer instead. See https://www.lawbster.de/datenschutz-kontaktformular/, https://www.it-recht-kanzlei.de/checkbox-online-shop-fallkonstellationen.html#abschnitt_38

--- includes/admin/settings/class-wc-gzd-settings-tab-general.php
+++ includes/admin/settings/class-wc-gzd-settings-tab-general.php
@@ -44,8 +44,6 @@ class WC_GZD_Settings_Tab_General extends WC_GZD_Settings_Tab {
 	}
 
 	protected function get_legal_page_settings() {
-		$additional_data_privacy_info = sprintf( __( 'Checkbox status: %s', 'woocommerce-germanized' ), WC_GZD_Admin_Legal_Checkboxes::instance()->get_terms_policy_status_html() );
-
 		return array(
 			array( 'title' => '', 'type' => 'title', 'id' => 'legal_page_options' ),
 
@@ -97,8 +95,7 @@ class WC_GZD_Settings_Tab_General extends WC_GZD_Settings_Tab {
 				'type'     => 'single_select_page',
 				'default'  => '',
 				'class'    => 'wc-enhanced-select-nostd',
-				'css'      => 'min-width:300px;',
-				'desc'     => '<div class="wc-gzd-additional-desc">' . $additional_data_privacy_info . '</div>',
+				'css'      => 'min-width:300px;'
 			),
 			array(
 				'title'    => __( 'Payment Methods', 'woocommerce-germanized' ),
