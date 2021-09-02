dc9b09930d358f4c66f42929817d7ff90e82a091
Dennis Nissle <info@vendidero.de>
2021-09-02 15:11:27 +0200

    Adding /vendor directory to release

---
+++ vendor/dvdoug/boxpacker/tests/Test/ConstrainedPlacementByCountTestItem.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Box packing (3D bin packing, knapsack problem).
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace DVDoug\BoxPacker\Test;
+
+use function array_filter;
+use function count;
+use DVDoug\BoxPacker\Box;
+use DVDoug\BoxPacker\ConstrainedPlacementItem;
+use DVDoug\BoxPacker\PackedItem;
+use DVDoug\BoxPacker\PackedItemList;
+use function iterator_to_array;
+
+class ConstrainedPlacementByCountTestItem extends TestItem implements ConstrainedPlacementItem
+{
+    /**
+     * @var int
+     */
+    public static $limit = 3;
+
+    /**
+     * Hook for user implementation of item-specific constraints, e.g. max <x> batteries per box.
+     */
+    public function canBePacked(
+        Box $box,
+        PackedItemList $alreadyPackedItems,
+        int $proposedX,
+        int $proposedY,
+        int $proposedZ,
+        int $width,
+        int $length,
+        int $depth
+    ): bool {
+        $alreadyPackedType = array_filter(
+            iterator_to_array($alreadyPackedItems, false),
+            function (PackedItem $item) {
+                return $item->getItem()->getDescription() === $this->getDescription();
+            }
+        );
+
+        return count($alreadyPackedType) + 1 <= static::$limit;
+    }
+}
