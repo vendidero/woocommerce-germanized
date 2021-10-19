01990f848af0f02bd8dc7f7c998e6e7399cc2162
Dennis Nissle <info@vendidero.de>
2021-10-19 14:10:14 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,159 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '31ab3f3576c7e476132c7b82fcd49249745ab5e3',
+    'name' => 'vendidero/woocommerce-germanized',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => '2.10.1',
+      'version' => '2.10.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '20393c4677765c3e737dcb5aee7a3f7b90dce4b3',
+    ),
+    'baltpeter/internetmarke-php' => 
+    array (
+      'pretty_version' => 'v0.5.0',
+      'version' => '0.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7657ee5a55eb80c77a35e35ce71c465364d73ab4',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.12.0',
+      'version' => '1.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+    ),
+    'dvdoug/boxpacker' => 
+    array (
+      'pretty_version' => '3.9.3',
+      'version' => '3.9.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9063023729c403e3c20a7635663f29fac569c96',
+    ),
+    'myclabs/deep-copy' => 
+    array (
+      'pretty_version' => '1.10.2',
+      'version' => '1.10.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '776f831124e9c62e1a2c601ecc52e776d8bb7220',
+      'replaced' => 
+      array (
+        0 => '1.10.2',
+      ),
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'setasign/fpdf' => 
+    array (
+      'pretty_version' => '1.8.4',
+      'version' => '1.8.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b0ddd9c5b98ced8230ef38534f6f3c17308a7974',
+    ),
+    'setasign/fpdi' => 
+    array (
+      'pretty_version' => 'v2.3.6',
+      'version' => '2.3.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6231e315f73e4f62d72b73f3d6d78ff0eed93c31',
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'vendidero/one-stop-shop-woocommerce' => 
+    array (
+      'pretty_version' => 'v1.1.6',
+      'version' => '1.1.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e15b7a8ec378e7af1cc5eb0315b0b1422f527a24',
+    ),
+    'vendidero/woocommerce-germanized' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '31ab3f3576c7e476132c7b82fcd49249745ab5e3',
+    ),
+    'vendidero/woocommerce-germanized-dhl' => 
+    array (
+      'pretty_version' => 'v1.6.2',
+      'version' => '1.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c0cb29c298a29be64662c1174abf2f787565869b',
+    ),
+    'vendidero/woocommerce-germanized-shipments' => 
+    array (
+      'pretty_version' => 'v1.6.2',
+      'version' => '1.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e0e0e9968ca71d64119cb82c36236a386fecbeb7',
+    ),
+    'vendidero/woocommerce-trusted-shops' => 
+    array (
+      'pretty_version' => 'v4.0.12',
+      'version' => '4.0.12.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c31be4a528bcb13b5d8cd842ec20efa469e65662',
+    ),
+    'wsdltophp/wssecurity' => 
+    array (
+      'pretty_version' => '1.2.2',
+      'version' => '1.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a450af3cd462cbf73fdb3a09e80322da893af4f',
+    ),
+  ),
+);
---
+++ vendor/dvdoug/boxpacker/tests/Test/ConstrainedPlacementNoStackingTestItem.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Box packing (3D bin packing, knapsack problem).
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace DVDoug\BoxPacker\Test;
+
+use function array_filter;
+use DVDoug\BoxPacker\Box;
+use DVDoug\BoxPacker\ConstrainedPlacementItem;
+use DVDoug\BoxPacker\PackedItem;
+use DVDoug\BoxPacker\PackedItemList;
+use function iterator_to_array;
+
+class ConstrainedPlacementNoStackingTestItem extends TestItem implements ConstrainedPlacementItem
+{
+    /**
+     * Hook for user implementation of item-specific constraints, e.g. max <x> batteries per box.
+     */
+    public function canBePacked(
+        Box $box,
+        PackedItemList $alreadyPackedItems,
+        int $proposedX,
+        int $proposedY,
+        int $proposedZ,
+        int $width,
+        int $length,
+        int $depth
+    ): bool {
+        $alreadyPackedType = array_filter(
+            iterator_to_array($alreadyPackedItems, false),
+            function (PackedItem $item) {
+                return $item->getItem()->getDescription() === $this->getDescription();
+            }
+        );
+
+        /** @var PackedItem $alreadyPacked */
+        foreach ($alreadyPackedType as $alreadyPacked) {
+            if (
+                $alreadyPacked->getZ() + $alreadyPacked->getDepth() === $proposedZ &&
+                $proposedX >= $alreadyPacked->getX() && $proposedX <= ($alreadyPacked->getX() + $alreadyPacked->getWidth()) &&
+                $proposedY >= $alreadyPacked->getY() && $proposedY <= ($alreadyPacked->getY() + $alreadyPacked->getLength())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
