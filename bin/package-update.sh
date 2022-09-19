4656b41055d2b63a7c34ebed03048d83f572701b
vendidero <info@vendidero.de>
2022-09-19 14:20:51 +0200

    Package cleanup.

--- bin/package-update.sh
+++ bin/package-update.sh
@@ -47,7 +47,12 @@ rm -rf ./packages/one-stop-shop-woocommerce/vendor
 output 3 "Clean vendor dirs to save space..."
 
 rm -rf ./vendor/dvdoug/boxpacker/visualiser/*
+rm -rf ./vendor/dvdoug/boxpacker/docs/*
+rm -rf ./vendor/dvdoug/boxpacker/tests/*.php
+rm -rf ./vendor/dvdoug/boxpacker/tests/Test/*.php
 rm -rf ./vendor/dvdoug/boxpacker/tests/data/*
+rm -rf ./vendor/dvdoug/boxpacker/*.md
+rm -rf ./vendor/dvdoug/boxpacker/.github/*
 
 output 2 "Done!"
 
