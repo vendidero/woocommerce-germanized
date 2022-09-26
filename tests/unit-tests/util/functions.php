600eff6ecfb6430133ba803d078ed0437aee875c
vendidero <info@vendidero.de>
2022-09-26 10:38:56 +0200

    Shortcode removal testing.

--- tests/unit-tests/util/functions.php
+++ tests/unit-tests/util/functions.php
@@ -37,9 +37,12 @@ class WC_GZD_Tests_Util_Functions extends WC_GZD_Unit_Test_Case {
 		$this->assertEquals( '<p>Simple shortcode test.</p><p>Content clear</p>', $this->clean_newlines( $post_content ) );
 		$this->assertEquals( $post_org_id, $post->ID );
 
-		$post_content = wc_gzd_get_post_plain_content( $post_id, array( 'another_simple_shortcode' ) );
+		add_shortcode( 'another_simple_shortcode', function() {
+			return 'shortcode_wrapper';
+		} );
 
-		$this->assertEquals( '<p>Simple shortcode test.</p><p>[another_simple_shortcode]</p><p>Content clear</p>', $this->clean_newlines( $post_content ) );
+		$post_content = wc_gzd_get_post_plain_content( $post_id, array( 'another_simple_shortcode' ) );
+		$this->assertEquals( '<p>Simple shortcode test.</p>shortcode_wrapper<p>Content clear</p>', $this->clean_newlines( $post_content ) );
 
 		$post_id = wp_insert_post( array(
 			'post_content' => '<p>[simple_shortcode arg1="2"][inner_shortcode]Simple shortcode test.[/inner_shortcode][/simple_shortcode]</p><p>[simple_shortcode arg1="2"][inner_shortcode] Simple shortcode test.[/simple_shortcode]</p>',
@@ -49,5 +52,24 @@ class WC_GZD_Tests_Util_Functions extends WC_GZD_Unit_Test_Case {
 		$post_content = wc_gzd_get_post_plain_content( $post_id );
 
 		$this->assertEquals( '<p>Simple shortcode test.</p><p> Simple shortcode test.</p>', $this->clean_newlines( $post_content ) );
+
+		$post_id = wp_insert_post( array(
+			'post_content' => '<p>Does it work?</p>',
+			'post_title'   => 'test',
+		) );
+
+		add_filter( 'the_content', function( $content ) {
+			return $content . '[test]';
+		}, 5000 );
+
+		$post = get_post( $post_id );
+		$content = $post->post_content;
+		$content = apply_filters( 'the_content', $content );
+
+		$this->assertEquals( '<p>Does it work?</p>[test]', $this->clean_newlines( $content ) );
+
+		$post_content = wc_gzd_get_post_plain_content( $post_id );
+
+		$this->assertEquals( '<p>Does it work?</p>', $this->clean_newlines( $post_content ) );
 	}
 }
