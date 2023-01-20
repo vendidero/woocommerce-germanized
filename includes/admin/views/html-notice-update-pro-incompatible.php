199f4bb65238ce4c835dc2c014bee7c450585edc
vendidero <info@vendidero.de>
2023-01-20 11:03:20 +0100

    Improve incompatibility notice display.

--- includes/admin/views/html-notice-update-pro-incompatible.php
+++ includes/admin/views/html-notice-update-pro-incompatible.php
@@ -3,12 +3,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<style>
-	.plugins p.wc-gzd-update-warning {
-		border-top: 1px solid #ffb900; margin-left: -12px; margin-right: -12px; padding: 12px; padding-bottom: 3px;
-	}
-	.plugins p.wc-gzd-update-warning::before {
-		display: none;
-	}
-</style>
 <p class="wc-gzd-update-warning"><?php echo wp_kses_post( sprintf( __( '<strong>Be aware!</strong> This update is not compatible with your current Germanized Pro version. Please <a href="%1$s">check for updates</a> before updating Germanized to prevent <a href="%2$s">compatibility issues</a>.', 'woocommerce-germanized' ), 'https://vendidero.de/dokument/germanized-pro-aktualisieren', 'https://vendidero.de/dokument/wichtige-plugins-fehlen-sind-veraltet-oder-werden-nicht-unterstuetzt' ) ); ?>
