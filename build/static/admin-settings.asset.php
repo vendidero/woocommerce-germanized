db667874a15907f4dfacc4686d93d5eaaef76ec6
vendidero <info@vendidero.de>
2023-10-24 13:06:20 +0200

    Adding /build directory to release

---
+++ build/static/admin-settings.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '0d7e08aa41a0a59c36df');
---
+++ build/static/direct-debit.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'bfdec22ea5531734f57f');
