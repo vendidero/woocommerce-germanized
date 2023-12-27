8bc6c6cea9c81801c2ce211e49eac3ba1d60898a
vendidero <info@vendidero.de>
2023-12-27 13:33:31 +0100

    Adding /build directory to release

---
+++ build/checkout-style.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wc-blocks-checkout', 'wc-blocks-data-store', 'wc-gzd-blocks-settings', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'ac6027d8779ac369b420');
---
+++ build/mini-cart-style.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-data-store', 'wc-gzd-blocks-settings', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fe623ef76ded15ade546');
