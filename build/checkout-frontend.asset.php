b7840d47db3f2ea9a52ccb675ff71f8b8a60ae76
vendidero <info@vendidero.de>
2024-04-12 14:03:20 +0200

    Adding /build directory to release

---
+++ build/checkout-frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wc-blocks-checkout', 'wc-blocks-data-store', 'wc-gzd-blocks-settings', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3a4d8300cf1946548456');
---
+++ build/static/admin-wizard-styles.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3c58dbdd7968d50f3baf');
