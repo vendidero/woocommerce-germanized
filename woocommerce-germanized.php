3613ab8b71cfb93fcf79f7af1565daf98e587473
vendidero <info@vendidero.de>
2024-05-23 15:59:51 +0200

    Tested up to update.

--- woocommerce-germanized.php
+++ woocommerce-germanized.php
@@ -9,7 +9,7 @@
  * Requires at least: 5.4
  * Tested up to: 6.5
  * WC requires at least: 3.9
- * WC tested up to: 8.7
+ * WC tested up to: 8.9
  *
  * Text Domain: woocommerce-germanized
  * Domain Path: /i18n/languages/
