21ff978c39d68cc29809d5e862db9349fd6c1740
vendidero <info@vendidero.de>
2024-05-31 15:05:49 +0200

    Adding /build directory to release

---
+++ build/cart-style.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-gzd-blocks-settings', 'wp-element', 'wp-plugins'), 'version' => '0b59fdf62938efde53ff');
---
+++ build/static/unit-price-observer-bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '146cbbb6f27bf289fb55');
