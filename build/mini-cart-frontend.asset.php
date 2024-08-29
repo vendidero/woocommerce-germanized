988bd857944982a4dc4ac97250dc658b3f2f17aa
vendidero <info@vendidero.de>
2024-08-29 12:03:46 +0200

    Adding /build directory to release

---
+++ build/mini-cart-frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-data-store', 'wc-gzd-blocks-settings', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5052ad3173cf2d92e15b');
---
+++ build/static/direct-debit.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '674078196df3e93c5286');
