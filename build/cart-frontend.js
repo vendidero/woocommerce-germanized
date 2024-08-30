86260233a6058e16aa23c9ec0ff9655fb1da507d
vendidero <info@vendidero.de>
2024-08-30 14:24:16 +0200

    Adding /build directory to release

---
+++ build/cart-frontend.js
@@ -0,0 +1,1 @@
+!function(){var t={528:function(){}},n={};function o(r){var e=n[r];if(void 0!==e)return e.exports;var i=n[r]={exports:{}};return t[r](i,i.exports,o),i.exports}!function(){"use strict";window.wc.blocksCheckout,window.wp.element,o(528)}()}();
---
+++ build/static/admin-product-variations.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '0527102131a6ba6ea39c');
