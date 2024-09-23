90ad662ad83abc998816bd6fcf1c98c97fa9ff4f
vendidero <info@vendidero.de>
2024-09-23 17:45:52 +0200

    tests

--- tests/unit-tests/api/products.php
+++ tests/unit-tests/api/products.php
@@ -98,7 +98,7 @@ class WC_GZD_Products_API extends WC_GZD_REST_Unit_Test_Case {
 		$nutrient   = wp_insert_term( 'Salt', 'product_nutrient', array( 'slug' => 'salt' ) );
 		$nutrient_2 = wp_insert_term( 'Natrium', 'product_nutrient', array( 'slug' => 'natrium' ) );
 		$allergen   = wp_insert_term( 'Nut', 'product_allergen', array( 'slug' => 'nut' ) );
-		$attachment_id = WC_GZD_Helper_Product::create_attachment( 'test' );
+		// $attachment_id = WC_GZD_Helper_Product::create_attachment( 'test' );
 
 		// Create simple.
 		$request = new WP_REST_Request( 'POST', '/wc/v3/products' );
