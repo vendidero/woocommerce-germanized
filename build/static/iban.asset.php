f2dee06f536c3e43235b44c38945df61d45946a6
vendidero <info@vendidero.de>
2025-03-17 10:47:00 +0100

    Adding /build directory to release

---
+++ build/static/iban.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7411408ef0a01eb00ab1');
---
+++ build/static/unit-price-observer.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3ed716aa78da7f0eb94e');
