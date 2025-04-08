7eca49a4dfe85698713a1c378d14bd3c4aa8fac0
vendidero <info@vendidero.de>
2025-04-08 14:52:12 +0200

    Adding /vendor directory to release

---
+++ vendor/dvdoug/boxpacker/src/LimitedSupplyBox.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Box packing (3D bin packing, knapsack problem).
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace DVDoug\BoxPacker;
+
+/**
+ * A "box" (or envelope?) to pack items into with limited supply.
+ */
+interface LimitedSupplyBox extends Box
+{
+    /**
+     * Quantity of boxes available.
+     */
+    public function getQuantityAvailable(): int;
+}
