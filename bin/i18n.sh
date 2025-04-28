098d60f84c80135740866d50712a30dd3cde5456
vendidero <info@vendidero.de>
2025-04-28 12:55:07 +0200

    i18n.

--- bin/i18n.sh
+++ bin/i18n.sh
@@ -16,16 +16,16 @@ output() {
 }
 
 # Use wp i18n to make sure JS build files are parsed instead of non-compiled assets (PoEdit does not support that)
-wp i18n make-pot --ignore-domain ./packages/woocommerce-germanized-dhl ./packages/woocommerce-germanized-dhl/i18n/woocommerce-germanized-dhl.pot --exclude="assets/,.github,release/,lib/"
-wp i18n make-pot --ignore-domain ./packages/woocommerce-germanized-shipments ./packages/woocommerce-germanized-shipments/i18n/woocommerce-germanized-shipments.pot --exclude="assets/,.github,release/,lib/"
+wp i18n make-pot --ignore-domain ./packages/shiptastic-integration-for-dhl ./packages/shiptastic-integration-for-dhl/i18n/shiptastic-integration-for-dhl.pot --exclude="assets/,.github,release/,lib/"
+wp i18n make-pot --ignore-domain ./packages/shiptastic-for-woocommerce ./packages/shiptastic-for-woocommerce/i18n/shiptastic-for-woocommerce.pot --exclude="assets/,.github,release/,lib/"
 
 output 3 "Update i18n script paths in POT file to make sure WP is able to load the right translations from packages dir"
-sed -i '' -e "s/\#: build\//\#: packages\/woocommerce-germanized-dhl\/build\//g" ./packages/woocommerce-germanized-dhl/i18n/woocommerce-germanized-dhl.pot
-sed -i '' -e "s/\#: build\//\#: packages\/woocommerce-germanized-shipments\/build\//g" ./packages/woocommerce-germanized-shipments/i18n/woocommerce-germanized-shipments.pot
+sed -i '' -e "s/\#: build\//\#: packages\/shiptastic-integration-for-dhl\/build\//g" ./packages/shiptastic-integration-for-dhl/i18n/shiptastic-integration-for-dhl.pot
+sed -i '' -e "s/\#: build\//\#: packages\/shiptastic-for-woocommerce\/build\//g" ./packages/shiptastic-for-woocommerce/i18n/shiptastic-for-woocommerce.pot
 output 2 "Done"
 
 # Create Germanized Pro POT file and merge with StoreaBill
-wp i18n make-pot ./ ./i18n/languages/woocommerce-germanized.pot --ignore-domain --exclude="assets/,release/,lib/" --merge="./packages/woocommerce-germanized-dhl/i18n/woocommerce-germanized-dhl.pot,./packages/woocommerce-germanized-shipments/i18n/woocommerce-germanized-shipments.pot"
+wp i18n make-pot ./ ./i18n/languages/woocommerce-germanized.pot --ignore-domain --exclude="assets/,release/,lib/" --merge="./packages/shiptastic-integration-for-dhl/i18n/shiptastic-integration-for-dhl.pot,./packages/shiptastic-for-woocommerce/i18n/shiptastic-for-woocommerce.pot"
 
 # Run composer update to make sure POT file paths are being updated
 composer update
