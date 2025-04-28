0c7fb359953ff6e86792b7748577b0aedf7750d9
vendidero <info@vendidero.de>
2025-04-28 12:58:17 +0200

    Adding /build directory to release

---
+++ build/static/layout-styles.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '74c4660d85b7507f13d8');
---
+++ build/static/unit-price-observer.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '391fb6d2ce023c18687e');
