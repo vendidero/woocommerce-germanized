ef74c4b0e8ba69d76126bafe0a2745dc657feecd
vendidero <info@vendidero.de>
2025-10-21 14:50:29 +0200

    Adding /build directory to release

---
+++ build/static/admin-styles.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2d244a498212f2e34d7e');
---
+++ build/static/admin-wizard-styles.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '43ea64f67eb2b3f25ff9');
